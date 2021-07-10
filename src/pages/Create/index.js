9bf77232082923568a65b2d8e879d0e81606356c
Walidnurudin <walidnurudin47@gmail.com>
2021-07-10 20:03:37 +0700

    Create screen

--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -1,12 +1,50 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { TextInput, View, StyleSheet, Button } from "react-native";
 
-export default class Create extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Create Screen</Text>
-            </View>
-        )
+const Create = () => {
+    const [title, setTitle] = React.useState("");
+    const [snippet, setSnippet] = React.useState("");
+    const [body, setBody] = React.useState("");
+
+    const Post = () => {
+        alert("ASUUUUU")
     }
-}
+
+    return (
+        <View>
+            <TextInput
+                style={styles.input}
+                onChangeText={setTitle}
+                value={title}
+                placeholder="Input Title"
+            />
+            <TextInput
+                style={styles.input}
+                onChangeText={setSnippet}
+                value={snippet}
+                placeholder="Input Snippet"
+            />
+            <TextInput
+                style={styles.input}
+                onChangeText={setBody}
+                value={body}
+                placeholder="Input Body"
+            />
+            <Button
+                title="Create"
+                onPress={() => Post()}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        borderColor: 'black',
+        borderRadius: 5,
+        borderWidth: 2,
+        margin: 5
+    }
+})
+
+export default Create;
