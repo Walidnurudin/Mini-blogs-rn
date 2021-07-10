a4a89b46443fca9bb1454481436b7f18d32bb354
Walidnurudin <walidnurudin47@gmail.com>
2021-07-10 19:22:12 +0700

    GET all blogs

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,7 +1,21 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 export default class Detail extends Component {
+    constructor(){
+        super();
+        this.state = {
+            data: []
+        }
+    }
+
+    deleteBlog(id) {
+        axios.delete('https://mini-blogs-app.herokuapp.com/api/blogs/' + id)
+            .then(res => res.data)
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <View>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
-
+import axios from 'axios';
 
 export default class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://mini-blogs-app.herokuapp.com/api/blogs')
+            .then(res => res.data)
+            .then(res => {
+                this.setState({
+                    data: res
+                })
+                console.log(res)
+            })
+            .catch(err => console.log(err));
+    }
 
     render() {
         return (
             <View>
                 <Text>Home screen</Text>
+                <View>
+                    {this.state.data.map((item, index) => {
+                        return (
+                            <View key={index} style={{borderBottomWidth: 3, padding: 5}}>
+                                <Text style={{
+                                    fontSize: 30
+                                }}>
+                                    {item.title}
+                                </Text>
+                                <Text>
+                                    {item.snippet}
+                                </Text>
+                            </View>
+                        )
+                    })}
+                </View>
                 <Button
                     title="Detail"
                     onPress={() => this.props.navigation.navigate('Detail')}
